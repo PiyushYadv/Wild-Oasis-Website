44f658d255694d41af7cae29ca620ae9aed320b1
Piyush Yadav <piyush.y276@gmail.com>
2026-05-01 23:52:54 +0530

    feat(app): add starter files and configure tailwind

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,14 @@
-export default function Page() {
-  return <h1>Cabins page</h1>;
+export default async function Page() {
+  const data = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await data.json();
+  return (
+    <div>
+      <h1>Cabins page</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
