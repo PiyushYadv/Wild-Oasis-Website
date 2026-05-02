585e996eb70033090216df08f6cbb6a4a638757e
Piyush Yadav <piyush.y276@gmail.com>
2026-05-02 22:01:27 +0530

    styles(app): add tailwind classes to improve UI

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,29 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
+import Header from "@/app/_components/Header";
 import "@/app/_styles/globals.css";
 
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
+
 export const metadata = {
-  title: "The Wild Oasis",
-  description: "Discover your next adventure",
+  title: {
+    template: "%s | The Wild Oasis",
+    default: "The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of Italian Dolomites, surrounded by beautiful mountains and dark forest.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
+      <body
+        className={`${josefin.className} antialiased bg-primary-950 text-gray-50 min-h-screen flex flex-col relative`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
         <footer>Copyright by the Wild Oasis</footer>
       </body>
     </html>
