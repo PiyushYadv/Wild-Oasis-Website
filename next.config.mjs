e2318b2d3a7a110596b1c474682d37bd3a43b10b
Piyush Yadav <piyush.y276@gmail.com>
2026-05-05 18:21:05 +0530

    configure next for remote images

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,15 @@ const nextConfig = {
   /* config options here */
   reactCompiler: true,
   images: {
-    qualities: [100, 75],
+    qualities: [100, 80, 75],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "eictbbtfxxjtoztamnuc.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
   },
 };
 
