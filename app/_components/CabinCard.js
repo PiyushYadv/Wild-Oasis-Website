55512c46b46411ea4f41870b5f295bc8e30540c0
Piyush Yadav <piyush.y276@gmail.com>
2026-05-06 23:02:07 +0530

    add loading=eager to image

--- app/_components/CabinCard.js
+++ app/_components/CabinCard.js
@@ -7,7 +7,7 @@ function CabinCard({ cabin }) {
 
   return (
     <div className="flex border-primary-800 border">
-      <div className="flex-1 relative">
+      <div className="flex-1 relative min-h-auto">
         <Image
           src={image}
           fill
