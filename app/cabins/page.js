88c0ad55f03494a0e2c008db753a59a05cbb1ed1
Piyush Yadav <piyush.y276@gmail.com>
2026-05-13 23:25:56 +0530

    feat(cabins): add Filter component and filtering to cabins

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,6 +1,7 @@
 import CabinList from "@/app/_components/CabinList";
 import { Suspense } from "react";
 import Spinner from "@/app/_components/Spinner";
+import Filter from "@/app/_components/Filter";
 
 export const metadata = {
   title: "Cabins",
@@ -8,7 +9,10 @@ export const metadata = {
 
 export const revalidate = 3600; // 1 hr
 
-export default async function Page() {
+export default async function Page({ searchParams }) {
+  const params = await searchParams;
+  const filter = params.capacity || "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -22,8 +26,11 @@ export default async function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
       <Suspense fallback={<Spinner />}>
-        <CabinList />
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
