08b65e7380b5eb1e6bf289cfee0ecbea69a2a817
Piyush Yadav <piyush.y276@gmail.com>
2026-05-21 18:38:10 +0530

    feat(components): Add username and profile photo to header

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,9 @@
 import Link from "next/link";
+import { auth } from "@/app/_lib/auth";
+import Image from "next/image";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
@@ -21,12 +24,29 @@ export default function Navigation() {
           </Link>
         </li>
         <li>
-          <Link
-            href="/account"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Guest area
-          </Link>
+          {session?.user?.image ? (
+            <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors flex items-center gap-4"
+            >
+              <Image
+                src={session.user.image}
+                alt={session.user.name}
+                width={32}
+                height={32}
+                className="w-8 h-8 rounded-full"
+                referrerPolicy="no-referrer"
+              />
+              <span>{session?.user?.name ?? "User"}</span>
+            </Link>
+          ) : (
+            <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors"
+            >
+              Guest area
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
