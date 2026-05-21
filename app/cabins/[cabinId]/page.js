631fc5e9ed06103263435b841fb7e2f536b8ab8c
Piyush Yadav <piyush.y276@gmail.com>
2026-05-21 18:42:02 +0530

    styles(cabin): Add margin bottom to text before reservation

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -36,7 +36,7 @@ export default async function Page({ params }) {
     <div className="max-w-6xl mx-auto mt-8">
       <Cabin cabin={cabin} />
       <div>
-        <h2 className="text-5xl font-semibold text-center">
+        <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400f">
           Reserve {cabin.name} today. Pay on arrival.
         </h2>
         <Suspense fallback={<Spinner />}>
