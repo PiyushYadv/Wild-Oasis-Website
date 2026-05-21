214880a892dcdb8ceb2320bdaf92fba2b9f44ddd
Piyush Yadav <piyush.y276@gmail.com>
2026-05-21 13:01:42 +0530

    feat(contexts): add ReservationProvider

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import Header from "@/app/_components/Header";
 import "@/app/_styles/globals.css";
-
 import { Josefin_Sans } from "next/font/google";
+import { ReservationProvider } from "@/app/_contexts/ReservationContext";
 
 const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
 
@@ -22,7 +22,9 @@ export default function RootLayout({ children }) {
       >
         <Header />
         <div className="flex-1 px-8 py-12 grid">
-          <main className="max-w-7xl mx-auto w-full">{children}</main>
+          <main className="max-w-7xl mx-auto w-full">
+            <ReservationProvider>{children}</ReservationProvider>
+          </main>
         </div>
         <footer>Copyright by the Wild Oasis</footer>
       </body>
