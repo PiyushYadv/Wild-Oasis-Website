33f1f42ed870810e70326f8c38bb52476d938dfb
Piyush Yadav <piyush.y276@gmail.com>
2026-05-30 00:50:42 +0530

    perf: Add image qualities and some remote urls for cors

--- next.config.mjs
+++ next.config.mjs
@@ -11,9 +11,12 @@ const nextConfig = {
         port: "",
         pathname: "/storage/v1/object/public/cabin-images/**",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
     ],
   },
-  output: "export",
 };
 
 export default nextConfig;
